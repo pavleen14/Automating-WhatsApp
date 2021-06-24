b81e179c8efd1c93b7bc443420e2549d2ca9c648
pavleen14 <pavonweb99@gmail.com>
2021-06-24 14:21:56 +0530

    sending an attachment

--- WhatsApp.py
+++ WhatsApp.py
@@ -10,6 +10,7 @@ browser = webdriver.Chrome('/Users/dell/Downloads/chromedriver')
 browser.get('https://web.whatsapp.com/')
 wait = WebDriverWait(browser, 600)
 
+#Sending a message to a single contact on WhatsApp
 target = '"Apna"' #target user
 string = "Message by python!" #target msg
 x_arg = ' //span[contains(@title, ' + target +')]'
@@ -20,3 +21,17 @@ input_box = browser.find_element_by_class_name('_2A8P4')
 
 for i in range(20):#loops runs for 100 times
     input_box.send_keys(string + Keys.ENTER)
+
+#Sending Image Attachment 
+file_path = "C:/Users/dell/OneDrive/Desktop/PHOTUU/github avatar.png"#file path
+from time import sleep#sending image to whatsapp
+attachment_section = browser.find_element_by_xpath('//div[@title = "Attach"]')
+attachment_section.click()
+
+image_box = browser.find_element_by_xpath('//input[@accept="image/*,video/mp4,video/3gpp,video/quicktime"]')
+image_box.send_keys(file_path)
+
+browser.implicitly_wait(40)
+
+send_button = browser.find_element_by_xpath('//span[@data-icon="send"]')
+send_button.click()
