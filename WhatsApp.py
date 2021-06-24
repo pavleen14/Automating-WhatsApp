83c7109018533c08f9b5187a6370637aa4b7f221
pavleen14 <pavonweb99@gmail.com>
2021-06-24 15:12:13 +0530

    sending msgs to multuple contacts

--- WhatsApp.py
+++ WhatsApp.py
@@ -5,11 +5,10 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
-
+from selenium.common.exceptions import NoSuchElementException
 browser = webdriver.Chrome('/Users/dell/Downloads/chromedriver')
 browser.get('https://web.whatsapp.com/')
 wait = WebDriverWait(browser, 600)
-
 #Sending a message to a single contact on WhatsApp
 target = '"Apna"' #target user
 string = "Message by python!" #target msg
@@ -35,3 +34,17 @@ browser.implicitly_wait(40)
 
 send_button = browser.find_element_by_xpath('//span[@data-icon="send"]')
 send_button.click()
+#sending messages to multiple contacts
+
+user_name_list = ['"Contact-1"','"Contact-2"','"Contact-3"'] #target contacts
+browser.implicitly_wait(40)
+
+for user_name in user_name_list:
+    string = "Good Morning, Hope you are doing well! This message is sent by an automated bot, kindly ignore it." #target msg
+    x_arg = ' //span[contains(@title, ' + user_name +')]'
+    user_name = wait.until(ec.presence_of_element_located((By.XPATH, x_arg)))
+    user_name.click()
+    input_box = browser.find_element_by_class_name('_2A8P4')
+
+    for i in range(1):#loops runs for 1 times
+        input_box.send_keys(string + Keys.ENTER)
